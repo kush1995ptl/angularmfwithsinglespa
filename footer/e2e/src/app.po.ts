42cadf08ea26c4258be957f80028ccc622da1d7a
Joel Denning <joeldenning@gmail.com>
2020-09-20 22:15:33 -0600

    Fixing naming conflict of root components

--- footer/e2e/src/app.po.ts
+++ footer/e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('footer-app-root .content span')).getText() as Promise<string>;
   }
 }
